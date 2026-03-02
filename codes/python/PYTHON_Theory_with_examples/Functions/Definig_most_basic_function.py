2d2c8afeea754147c6eb7d4c4c960c93bb13fe1a
Himanshu Jha <jhahimansu2007@gmail.com>
2026-03-02 07:03:16 +0545

    Delete codes/python/PYTHON_Theory_with_examples/Functions directory

--- codes/python/PYTHON_Theory_with_examples/Functions/Definig_most_basic_function.py
+++
@@ -1,8 +0,0 @@
-def greet_user(): # def is used to define function
-    print("Hi there!")
-    print("Welcome to my repo")
-
-
-print("Start")
-greet_user()   # Here we call function
-print("Finish")
--- codes/python/PYTHON_Theory_with_examples/Functions/Using_return.py
+++
@@ -1,4 +0,0 @@
-def square(Number):
-    return Number*Number
-Number=int(input("Enter the number you want to square the value:"))
-print(f"The square root of {Number} is {square(Number)}")
