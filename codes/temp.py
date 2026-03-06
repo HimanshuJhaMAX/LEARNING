47ecfbfcc73260f7c94ebec82e0baa4f1590de08
HimanshuJhaMAX <jhahimansu2007@gmail.com>
2026-03-06 20:53:56 +0545

    Learned classes

---
+++ codes/temp.py
@@ -0,0 +1,12 @@
+class Point:
+    def move(self):
+        print("Move")
+        
+    def draw(self):
+        print("Draw")
+        
+
+point1 = Point()
+point1.move()
+point2 = Point()
+point2.draw()
