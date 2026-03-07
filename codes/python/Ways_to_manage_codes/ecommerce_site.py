e5770b858fcdd48e8add8afdfded0eaf24768260
HimanshuJhaMAX <jhahimansu2007@gmail.com>
2026-03-07 20:28:46 +0545

    Learned packages

---
+++ codes/python/Ways_to_manage_codes/ecommerce_site.py
@@ -0,0 +1,8 @@
+from ecommerce_package.shipping import calc_shipping
+calc_shipping()
+#While working with packages we use the upper approach 
+#For information we got another approach down below
+
+
+#import ecommerce_package.shipping
+#ecommerce_package.shipping.calc_shipping()
